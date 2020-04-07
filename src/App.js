0d6a2434ddc96a7c0b17aad93de9eec420d98dcf
david <daviddecastro42@live.co.uk>
2020-04-07 20:48:43 +0100

    install oxios, fetich data from github to users componet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
-import User from './components/users/Users';
+import Users from './components/users/Users';
+import axios from 'axios';
 
 import './App.css';
 
 class App extends Component {
-  componentDidMount() {
-    console.log(123);
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
   }
 
   render() {
@@ -14,7 +24,7 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <User />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
